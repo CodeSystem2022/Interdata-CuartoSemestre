cfcca686f593af34300d8f86dcb5a208eaff79bb
francot03 <106187038+francot03@users.noreply.github.com>
2023-08-31 17:10:28 -0300

    agrego products.js

---
+++ Semana 1/JavaScript/products.js
@@ -0,0 +1,42 @@
+const productos = [
+  {
+    id: 1,
+    productName: "Atún",
+    quanty: 1,
+    price: 280,
+    image:"client/media/lataAtun.png",
+  },
+  
+  {
+    id: 2,
+    productName:"Pate",
+    quanty: 1,
+    price: 120,
+    image:"client/media/lataPate.png",
+  },
+
+  {
+    id: 3,
+    productName:"Manteca",
+    quanty:1,
+    price:500,
+    image:"client/media/manteca.png",
+  },
+
+  {
+    id: 4,
+    productName:"Leche",
+    quanty:1,
+    price:400,
+    image:"client/media/leche.png",
+  },
+
+  {
+    id: 5,
+    productName:"Arroz",
+    quanty:1,
+    price:200,
+    image:"client/media/paqArroz.png",
+  },
+  
+]
