71d3b122f93e7748b5f141b3e2dc24fdfa3cea3f
francot03 <106187038+francot03@users.noreply.github.com>
2023-08-31 17:18:52 -0300

    Agrego index.js

---
+++ Semana 1/JavaScript/index.js
@@ -0,0 +1,10 @@
+const shopContent = document.GetItemById("shopContent");
+
+productos.forEach ((product) =>{
+  const content = document.createElement("div");
+  content.innerHTML = `
+  <img src ="$(product.img)">
+  <h3>$(product.productName)</h3>
+  <p>$(product.price)</p>
+  `;
+});
