696e8a63dd70d314717e1c0835e8ea9e26523035
francot03 <106187038+francot03@users.noreply.github.com>
2023-09-13 23:33:37 -0300

    2.1 y 2.2 - Carrito y boton de compra

---
+++ Semana 2/JavaScript/index.js
@@ -0,0 +1,32 @@
+// Franco Bianchi
+
+const shopContent = document.getElementById("shopContent™ );
+const cart = [];
+
+productos.forEach((product) =>{
+  const content = document.createElement ("div") ;
+  const content = document.createElement("div");
+  content.innerHTML = `
+  <img src="${product.img}">
+  <h3>${product.productName}</h3>
+
+  <p>${product.price} $</p>
+  `;
+  shopContent.append(content);
+
+  const buyButton = document.createElement("button");
+  buyButton.innerText = "Comprar";
+
+  content.append(buyButton);
+
+  buyButton.addEventListener("click", ()=>{
+    cart.push({
+      id:product.id,
+      productName:product.productName,
+      price:product.price,
+      quanty:product.quanty,
+      img:product.img,
+    })
+    console.log(cart) //Para mostrar que genera cart
+  })
+});
