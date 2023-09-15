70ebf8cdfde7e71406bde5881f2c3f9add7fcbef
GabrielEchave <112594225+GabrielEchave@users.noreply.github.com>
2023-09-15 19:21:33 -0300

    Create tipo_str.py

---
+++ Semana 3/Python/tipo_str.py
@@ -0,0 +1,11 @@
+import math
+# Profundizando en el tipo String
+# Concatenacion automatica en Python
+
+variable = 'Adios'
+mensaje = 'Hola''Alumnos' + variable
+mensaje += ', Terminamos'
+# print(mensaje)
+
+# Usamos la clase help para ayuda o documentacion (built-in)
+help(math.isnan)
