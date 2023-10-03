0db8374c0e890ce82c16266a1cbfbbe50a45dc35
ignacio buchter <111471268+Naachooo@users.noreply.github.com>
2023-10-03 19:12:54 -0300

    .

--- Semana 4 /JavaScript/ cart.js
+++
@@ -1,9 +0,0 @@
-const displayCartCounter = () => {
-  const cartLength = cart.reduce((acc, el) => acc +el.quanty, 0);
-  if (cartLength< 0) {
-    cartCounter.style.display = "block";
-    cartCounter.innerText = cartLength;
-  } else {
-    cartCounter.style.display = "none";
-  }
-};
