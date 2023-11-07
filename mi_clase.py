f036977fb265e02b63ad5ff3aeebb4b0f7683b31
ignacio buchter <111471268+Naachooo@users.noreply.github.com>
2023-11-07 00:29:34 -0300

    Add files via upload

---
+++ mi_clase.py
@@ -0,0 +1,27 @@
+
+
+class MiClase:
+    '''
+    Este es un ejemplo de la documentación de esta clase
+    Es solo para saber que aqu esta mi clase
+    '''
+    def __init__(self):
+        '''
+        Aqui esta el metodo dunder innit
+        este metodo es el de inicio para esta clase
+        '''
+
+    def mi_metodo(self,param1, param2):
+        '''
+        Detecta los paraarametros y los agrega automaticamente
+        :param param1:
+        :param param2:
+        :return:
+        '''
+
+
+
+
+
+
+
