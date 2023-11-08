528bdfa0fd4a9c60a28192654c85dbb9f23a72a1
Gabriel Echave <112594225+GabrielEchave@users.noreply.github.com>
2023-11-08 12:29:49 -0300

    Create tareas.routes.js

---
+++ Semana 6/JavaScript/src/router/tareas.routes.js
@@ -0,0 +1,22 @@
+import Router from "express-promise-router";
+import {
+    actualizarTarea,
+    crearTarea,
+    eliminarTarea,
+    listarTarea,
+    listarTareas
+} from "../controllers/tareas.controller.js";
+import { isAuth } from "../middlewares/auth.middlaware.js";
+const router = Router();
+
+router.get("/tareas", isAuth, listarTareas);
+
+router.get("/tareas/:id", isAuth, listarTarea);
+
+router.post("/tareas", isAuth, crearTarea);
+
+router.put("/tareas/:id", isAuth, actualizarTarea);
+
+router.delete("/tareas/:id", isAuth, eliminarTarea);
+
+export default router;
