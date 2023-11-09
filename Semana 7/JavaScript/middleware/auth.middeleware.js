f02f413b2f861762e6b5d326d8060dcbb2e8ed94
Gabriel Echave <112594225+GabrielEchave@users.noreply.github.com>
2023-11-09 20:33:15 -0300

    Create auth.middeleware.js

---
+++ Semana 7/JavaScript/middleware/auth.middeleware.js
@@ -0,0 +1,19 @@
+export const isAuth = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "No estas autorizado",
+    });
+  }
+
+  jwt.verify(token, "xyz123", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        message: "No estas autorizado",
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
