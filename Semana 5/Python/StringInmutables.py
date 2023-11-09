ac7f37add5b20d2319cff2fa5a7d5e4d6b8f4d95
Silvina Prado <112594123+SilPrad0@users.noreply.github.com>
2023-11-09 09:43:35 -0300

    Update StringInmutables.py

--- Semana 5/Python/StringInmutables.py
+++ Semana 5/Python/StringInmutables.py
@@ -1,1 +1,11 @@
 
+#help(str.capitalize)
+
+mensaje1 = 'Hola mundo'
+mensaje2 = mensaje1.capitalize()
+print(f'Mensaje1: {mensaje1}, id {id:(mensaje1)}')
+print(f'Mensaje2: {mensaje2}, id {id:(mensaje2)}')
+
+mensaje1 += 'Adios'
+print(f'Mensaje1: {mensaje1}, id {id:(mensaje1)}')
+
