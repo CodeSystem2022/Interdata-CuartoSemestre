772472a675c4ecd78041bfb50fc2903f10cf57a7
Silvina Prado <112594123+SilPrad0@users.noreply.github.com>
2023-11-09 09:56:21 -0300

    Create TiendaLibros.java

---
+++ Semana 6/TiendaLibros.java
@@ -0,0 +1,13 @@
+package utn.tienda_libros;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@SpringBootApplication
+public class TiendaLibrosApplication {
+
+    public static void main(String[] args) {SpringApplication.run(TiendaLibrosApplication.class, args);
+    }
+}
