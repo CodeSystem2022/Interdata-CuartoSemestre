57117e1a78462aa4b9df305768f964ed4ba832ea
Gabriel Echave <112594225+GabrielEchave@users.noreply.github.com>
2023-11-11 18:56:25 -0300

    Update TiendaLibrosApplication.java

--- Semana 8/Java/TiendaLibrosApplication.java
+++ Semana 8/Java/TiendaLibrosApplication.java
@@ -1,12 +1,10 @@
 package utn.tienda_libros;
 
-import org.springframework.boot.ConfigurableBootstrapContext;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-import utn.tienda_libros.vista.LibroFrom;
+import utn.tienda_libros.vista.LibroForm;
 
 import java.awt.*;
 
@@ -17,5 +15,15 @@ public class TiendaLibrosApplication {
 
 		ConfigurableApplicationContext contextoSpring =
 				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+
+		//Ejecitamos el código para cargar el formulario
+		EventQueue.invokeLater(() -> { //Método Lambda
+			//Obtenemos el objeto form a través del spring
+			LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+			libroForm.setVisible(true);
+		});
 	}
 }
